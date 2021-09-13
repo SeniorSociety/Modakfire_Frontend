45f6a7d92fdf9fda4c4490cc038aeae86287f894
택준 <taekjunsim@gmail.com>
2021-09-13 13:25:25 +0900

    initial commit

---
+++ src/Routes.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function Routes() {
+	return (
+		<Router>
+			<Switch>
+				{/* <Route exact path="/" component={HomePage} /> */}
+				{/* <Route exact path="/" component={HomePage} /> */}
+			</Switch>
+		</Router>
+	);
+}
+
+export default Routes;
