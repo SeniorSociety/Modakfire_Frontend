5849b31f67a4e064884f04628f92045a0ff6fe95
nam2350 <nam2350@gmail.com>
2021-09-13 13:58:09 +0900

    Add: SignIn
    Add: SignIn Page 완성

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn/SignIn';
 
 function Routes() {
 	return (
 		<Router>
 			<Switch>
-				{/* <Route exact path="/" component={HomePage} /> */}
+				<Route exact path="/signin" component={SignIn} />
 				{/* <Route exact path="/" component={HomePage} /> */}
 			</Switch>
 		</Router>
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import KakaoLogin from 'react-kakao-login';
+import './Signin.scss';
 
 function SignIn() {
-	return <div></div>;
+	const onLoginKakao = (data: any) => {
+		const accessToken = data.response.access_token;
+		const headers = {
+			Authorization: accessToken,
+		};
+		console.log('res data', data);
+
+		// axios
+		// 	.post('', { headers })
+		// 	.then(res => console.log('카카오 로그인 res', res))
+		// 	.catch(err => console.error);
+	};
+	return (
+		<div className="wrapper">
+			<div className="contentContainer">
+				<div className="logoBox">
+					<img className="logo" src="./images/senior-care-logo.png" alt="모닥불 로고" />
+				</div>
+				<div className="socialLoginContainer">
+					<p className="socialLoginText">간편 로그인</p>
+					<div>
+						<img className="naverLogin" src="./images/naver_login.png" alt="네이버로그인" />
+					</div>
+					<KakaoLogin
+						token="01901cb663806af74a0c0c80b56895de"
+						onSuccess={result => onLoginKakao(result)}
+						onFail={(result: any) => console.log(result)}
+						render={(props: any) => (
+							<div onClick={props.onClick}>
+								<img src="./images/kakao_login.png" alt="카카오로그인" />
+							</div>
+						)}
+						needProfile={true}
+						style={{ cursor: 'pointer' }}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default SignIn;
