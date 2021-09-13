b725b484a33c5a19280f1e6bcfb642bc4aa5d9f4
소진수 <joshso121197@gmail.com>
2021-09-13 20:14:57 +0900

    [introduce] Add : layout, textarea

--- src/pages/NameCard/NameCard.tsx
+++ src/pages/NameCard/NameCard.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Introduce from '../../components/Introduce/Introduce';
 
 function NameCard() {
-	return <div></div>;
+	return (
+		<div>
+			<Introduce />
+		</div>
+	);
 }
 
 export default NameCard;
