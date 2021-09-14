e20a8c8d8b67980fd00e5b9c8a85e3f10a6d5511
Jinsoo So <79290170+joshhhso@users.noreply.github.com>
2021-09-14 19:18:44 +0900

    [history/css] modify : layout (#17)
    Co-authored-by: nam2350 <nam2350@gmail.com>

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import './History.scss';
 
 function History() {
-	return <div>이력</div>;
+	const edit: string = '';
+
+	return (
+		<>
+			<div className="historyContainer">
+				<div className="line"></div>
+
+				<header className="workTitle">
+					이력
+					<div className="square"></div>
+				</header>
+
+				{edit === 'edit' && (
+					<>
+						<section className="workContainer">
+							<div className="leftContainer">
+								<div className="yearContainer">
+									<span>2019</span>
+								</div>
+								<div className="yearIcon"></div>
+							</div>
+							<div className="rightContainer">
+								<div className="title">이사장</div>
+								<div className="subtitle">해오름재단 마포구 서울시</div>
+							</div>
+						</section>
+					</>
+				)}
+				{edit === '' && (
+					<>
+						<section className="workContainer">
+							<div className="leftContainer">
+								<div className="yearContainer">
+									<input type="text" className="yearInput" placeholder="1997"></input>
+								</div>
+								<div className="yearIcon"></div>
+							</div>
+							<div className="rightContainer">
+								<input type="text" className="title" placeholder="예시) 교장선생님" />
+								<input
+									type="text"
+									className="subtitle"
+									placeholder="예시) 시소중학교 마포구 서울시"
+								/>
+							</div>
+						</section>
+						<input type="button" className="add" value="추 가 하 기"></input>
+					</>
+				)}
+			</div>
+		</>
+	);
 }
 
 export default History;
