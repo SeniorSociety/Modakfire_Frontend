366aa335ee83c9c249499d1f6211b550ea5484b7
택준 <taekjunsim@gmail.com>
2021-09-15 15:06:56 +0900

    Add: axios 완료, fasle시 get중단

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import BoardList from './pages/BoardList/BoardList';
 
 function Routes() {
 	return (
 		<Router>
 			<Switch>
-				{/* <Route exact path="/" component={HomePage} /> */}
+				<Route exact path="/" component={BoardList} />
 				{/* <Route exact path="/" component={HomePage} /> */}
 			</Switch>
 		</Router>
---
+++ src/pages/BoardList/BoardList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import InfiniteScroll from '../../components/InfiniteScroll/InfiniteScroll';
+import './boardList.scss';
+
+function BoardList() {
+	return (
+		<div className="boardWrap">
+			<div className="buttonWrap">
+				<button className="like btn">즐겨찾기 추가</button>
+				<button className="write btn">게시글 작성하기</button>
+			</div>
+			<div className="contentWrap">
+				<InfiniteScroll />
+			</div>
+		</div>
+	);
+}
+
+export default BoardList;
