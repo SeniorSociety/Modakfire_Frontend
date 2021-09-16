2adda9cc97d84eecafe402a7de30c0f5d94e64f5
소진수 <joshso121197@gmail.com>
2021-09-16 13:58:30 +0900

    [history] modify: error fixed

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BoardPost from './pages/Board/BoardPost';
 import BoardViewer from './pages/Board/BoardViewer';
+import BoardList from './pages/BoardList/BoardList';
+import NameCard from './pages/NameCard/NameCard';
 
 function Routes() {
 	return (
@@ -8,6 +10,8 @@ function Routes() {
 			<Switch>
 				<Route exact path="/board-post" component={BoardPost} />
 				<Route exact path="/board-viewer" component={BoardViewer} />
+				<Route exact path="/board-list" component={BoardList} />
+				<Route exact path="/namecard" component={NameCard} />
 			</Switch>
 		</Router>
 	);
---
+++ src/components/History/History.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import './History.scss';
+interface Inputs {
+	id?: number;
+	year: string;
+	title: string;
+	subtitle: string;
+}
+
+function History() {
+	const [edit, setEdit] = useState(true);
+	const [addHistory, setAddHistory] = useState<number[]>([0]);
+	const [contents, setContents] = useState<Inputs>({
+		id: 0,
+		year: '',
+		title: '',
+		subtitle: '',
+	});
+
+	const [histories, setHistories] = useState<any[]>([]);
+
+	const onChangeHandler = (e: any) => {
+		const { name, value } = e.target;
+		setContents({ ...contents, [name]: value });
+	};
+
+	function createHistory(): void {
+		const history = {
+			year: contents.year,
+			title: contents.title,
+			subtitle: contents.subtitle,
+		};
+		setHistories([...histories, history]);
+		setContents({ year: '', title: '', subtitle: '' });
+		setAddHistory(addHistory.concat(1));
+	}
+
+	function editHistory() {
+		setEdit(!edit);
+	}
+
+	console.log('이력추가 ', histories);
+
+	return (
+		<>
+			<div className="historyContainer">
+				<div className="line"></div>
+				<header className="workTitle">
+					이력
+					<div className="square"></div>
+				</header>
+				{edit ? (
+					<>
+						{addHistory.map(history => {
+							return (
+								<section key={history} className="workContainer">
+									<div className="leftContainer">
+										<div className="yearContainer">
+											<input
+												name="year"
+												type="text"
+												className="yearInput"
+												placeholder="1997"
+												onChange={onChangeHandler}
+											></input>
+										</div>
+										<div className="yearIcon"></div>
+									</div>
+									<div className="rightContainer">
+										<input
+											name="title"
+											type="text"
+											className="title"
+											placeholder="예시) 교장선생님"
+											onChange={onChangeHandler}
+										/>
+										<input
+											name="subtitle"
+											type="text"
+											className="subtitle"
+											placeholder="예시) 시소중학교 마포구 서울시"
+											onChange={onChangeHandler}
+										/>
+									</div>
+								</section>
+							);
+						})}
+						<input
+							type="button"
+							className="add"
+							value="추 가 하 기"
+							onClick={createHistory}
+						></input>
+						<input type="button" className="add" value="완 료 하 기" onClick={editHistory}></input>
+					</>
+				) : (
+					<>
+						{histories.map((history, index: number) => {
+							return (
+								<section key={index} className="workContainer">
+									<div className="leftContainer">
+										<div className="yearContainer">
+											<span>{history.year}</span>
+										</div>
+										<div className="yearIcon"></div>
+									</div>
+									<div className="rightContainer">
+										<div className="title">{history.title}</div>
+										<div className="subtitle">{history.subtitle}</div>
+									</div>
+								</section>
+							);
+						})}
+						<input type="button" className="add" value="추 가 하 기" onClick={editHistory}></input>
+					</>
+				)}
+			</div>
+		</>
+	);
+}
+
+export default History;
