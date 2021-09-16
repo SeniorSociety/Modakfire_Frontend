201133733e11c5b00e684526c1536600fc22296f
소진수 <joshso121197@gmail.com>
2021-09-16 19:58:50 +0900

    h

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,54 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './header.scss';
 
 function Header() {
 	const test: string = 'login';
+	const history = useHistory();
+
+	function moveToMain(): void {
+		history.push('/');
+	}
+
+	function moveToLogin(): void {
+		history.push('/signin');
+	}
+
+	function moveToNamecard(): void {
+		history.push('/namecard');
+	}
 
 	return (
 		<nav className="headerContainer">
-			<div className="logo">
+			<div className="logo" onClick={moveToMain}>
 				<span>모닥불</span>
 			</div>
 			{test === 'login' && (
 				<>
-					<div className="signIn">
+					<div className="signIn" onClick={moveToLogin}>
 						<span>회원가입</span>
 					</div>
-					<div className="logIn">
+					<div className="logIn" onClick={moveToLogin}>
 						<span>로그인</span>
 					</div>
 				</>
 			)}
 			{test === 'logout' && (
 				<>
-					<div className="signIn">
+					<div className="signIn" onClick={moveToNamecard}>
 						<span>명함제작</span>
 					</div>
-					<div className="logIn">
+					<div className="logIn" onClick={moveToLogin}>
 						<span>로그아웃</span>
 					</div>
 				</>
 			)}
 			{test === 'namecard' && (
 				<>
-					<div className="signIn">
+					<div className="signIn" onClick={moveToNamecard}>
 						<span>명함보기</span>
 					</div>
-					<div className="logIn">
+					<div className="logIn" onClick={moveToLogin}>
 						<span>로그아웃</span>
 					</div>
 				</>
