5c052842fd83736419bd9e79acae8f3ccb87aa11
소진수 <joshso121197@gmail.com>
2021-09-17 18:52:03 +0900

    [namecard] Modify : post done

--- src/config.tsx
+++ src/config.tsx
@@ -1,9 +1,9 @@
-const BASE_URL = 'http://testalb-647104143.ap-northeast-2.elb.amazonaws.com';
+const BASE_URL = 'internal-RealALB-2052302361.ap-northeast-2.elb.amazonaws.com';
 
 export const API = {
 	SERVER: `${BASE_URL}`,
-	MAIN: `${BASE_URL}/`,
-	BOARD: `${BASE_URL}/galleries`,
+	MAIN: `${BASE_URL}/galleries`,
+	BOARD: `${BASE_URL}/galleries/:id`,
 	SIGNIN: `${BASE_URL}/signin`,
-	NAMECARD: `${BASE_URL}/namecard/:id`,
+	NAMECARD: `${BASE_URL}/namecard`,
 };
--- src/pages/NameCard/NameCard.tsx
+++ src/pages/NameCard/NameCard.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
+import { API } from '../../config';
 import Card from '../../components/Card/Card';
 import History from '../../components/History/History';
 import Introduce from '../../components/Introduce/Introduce';
 
 const NameCard = () => {
+	const history = useHistory();
 	const [edit, setEdit] = useState<boolean>(true);
 
 	const [userImage, setUserImage] = useState<string>('');
@@ -13,36 +16,73 @@ const NameCard = () => {
 	const [userEmail, setUserEmail] = useState<string>('');
 	const [userLocal, setUserLocal] = useState<string>('');
 
-	const [historyYear, setHistoryYear] = useState();
-	const [historyTitle, setHistoryTitle] = useState();
-	const [historySubtitle, setHistorySubtitle] = useState();
+	const [historyYear, setHistoryYear] = useState<string[]>([]);
+	const [historyTitle, setHistoryTitle] = useState<string[]>([]);
+	const [historySubtitle, setHistorySubtitle] = useState<string[]>([]);
 
-	const [introduce, setIntroduce] = useState();
+	const [introduce, setIntroduce] = useState<string>('');
 
 	const [data, setData] = useState();
 
 	useEffect(() => {
 		axios
-			.get('https://www.seso.kr/galleries')
+			.get('http://10.58.5.153:8000/users/namecard', {
+				headers: {
+					Authorization:
+						'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.Te32okoTxCk31WOFbT-LiVhTMcu_5IRPsEum3y930OQ',
+				},
+			})
 			.then(res => {
-				setData(res.data.Message);
+				setData(res.data.MESSAGE);
+				console.log(res);
 			})
 			.catch(error => {
 				console.log(error);
 			});
 	}, []);
 
-	function handleuploadData(e: any): void {}
+	function handleuploadData(e: any): void {
+		const formData = new FormData();
+		for (let i = 0; i < historyYear.length; i++) {
+			formData.append('historyYear', historyYear[i]);
+		}
+		for (let i = 0; i < historyTitle.length; i++) {
+			formData.append('historyTitle', historyTitle[i]);
+		}
+		for (let i = 0; i < historySubtitle.length; i++) {
+			formData.append('historySubtitle', historySubtitle[i]);
+		}
+		formData.append('userName', userName);
+		formData.append('userSlogan', userSlogan);
+		formData.append('userLocal', userLocal);
+		formData.append('userEmail', userEmail);
+		formData.append('introduce', introduce);
+		formData.append('userImage', userImage[0]);
+		const header = {
+			headers: {
+				Authorization:
+					'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.Te32okoTxCk31WOFbT-LiVhTMcu_5IRPsEum3y930OQ',
+				'Content-Type': 'multipart/form-data',
+			},
+		};
+		axios.post('http://10.58.5.153:8000/users/namecard', formData, header).then(res => {
+			history.push(`http://10.58.5.153:8000/users/namecard`);
+			setEdit(false);
+		});
 
+		alert('명함제작이 완료되었습니다');
+		console.log('namecard year', historyYear);
+		console.log('namecard title', historyTitle);
+		console.log('namecard subtitle', historySubtitle);
+	}
+
+	console.log(userImage);
 	return (
 		<div>
 			<Card
 				edit={edit}
+				data={data}
 				userImage={userImage}
-				userName={userName}
-				userSlogan={userSlogan}
-				userEmail={userEmail}
-				userLocal={userLocal}
 				setUserImage={setUserImage}
 				setUserName={setUserName}
 				setUserSlogan={setUserSlogan}
@@ -51,14 +91,17 @@ const NameCard = () => {
 			/>
 			<History
 				edit={edit}
-				historyYear={historyYear}
-				historyTitle={historyTitle}
-				historySubTitle={historySubtitle}
+				data={data}
 				setHistoryYear={setHistoryYear}
 				setHistoryTitle={setHistoryTitle}
 				setHistorySubTitle={setHistorySubtitle}
 			/>
-			<Introduce edit={edit} introduce={introduce} setIntroduce={setIntroduce} />
+			<Introduce
+				edit={edit}
+				introduce={introduce}
+				setIntroduce={setIntroduce}
+				handleUploadData={handleuploadData}
+			/>
 		</div>
 	);
 };
