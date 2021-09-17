dd6af42596b324604a1a544cb35c54b4bb4fa902
택준 <taekjunsim@gmail.com>
2021-09-17 13:25:40 +0900

    Modify: route config적용

--- src/config.tsx
+++ src/config.tsx
@@ -1,12 +1,11 @@
-const BASE_URL = 'internal-RealALB-2052302361.ap-northeast-2.elb.amazonaws.com';
+const BASE_URL = 'http://internal-RealALB-2052302361.ap-northeast-2.elb.amazonaws.com';
 
 export const API = {
 	SERVER: `${BASE_URL}`,
-
-	MAIN: `${BASE_URL}/galleries`,
-	BOARDLIST: `${BASE_URL}/galleries/:id`,
+	MAIN: `${BASE_URL}/`,
+	BOARDLIST: `${BASE_URL}/board-list/:id`,
 	BOARDPOST: `${BASE_URL}/board-post`,
-	BOARDVIEWER: `${BASE_URL}/galleries/:id/:id`,
+	BOARDVIEWER: `${BASE_URL}/galleries`,
 	SIGNIN: `${BASE_URL}/signin`,
 	NAMECARD: `${BASE_URL}/namecard/:id`,
 };
