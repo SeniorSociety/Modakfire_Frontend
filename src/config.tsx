625d62a31430724329b8cda31f9e36429e763bd2
nam2350 <nam2350@gmail.com>
2021-09-23 16:17:50 +0900

    jenkins test2

--- src/config.tsx
+++ src/config.tsx
@@ -1,10 +1,10 @@
 const BASE_URL = 'http://internal-BACK-Internal-ALB-1927362478.ap-northeast-2.elb.amazonaws.com';
 
 export const API = {
-	SERVER: `${BASE_URL}`,
-	MAIN: `${BASE_URL}/galleries`,
-	BOARD: `${BASE_URL}/galleries`,
-	BOARD_VIEW: `${BASE_URL}/board-viewer`,
-	SIGNIN: `${BASE_URL}/signin`,
-	NAMECARD: `${BASE_URL}/namecard`,
+	// SERVER: `${BASE_URL}`,
+	MAIN: `/galleries`,
+	BOARD: `/galleries`,
+	BOARD_VIEW: `/board-viewer`,
+	SIGNIN: `/signin`,
+	NAMECARD: `/namecard`,
 };
