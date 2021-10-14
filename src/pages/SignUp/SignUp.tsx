7e57d5f31e20e71e4fbb2e2a6b02e9729518a072
nam2350 <nam2350@gmail.com>
2021-10-14 19:13:06 +0900

    signup 기능

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation, useHistory } from 'react-router';
+import { Input, Button } from 'antd';
+import axios from 'axios';
 
 function SignUp() {
-	return <div className="wrapper"></div>;
+	const history = useHistory();
+	const location = useLocation();
+	const [inputNickname, setInputNickname] = useState('');
+
+	const handleInput = event => {
+		const { value } = event.target;
+		setInputNickname(value);
+	};
+
+	const onCreateNickname = () => {
+		axios.post('', { nickname: inputNickname }).then(res => {
+			console.log('닉네임등록', res.data);
+		});
+	};
+	console.log('location', location);
+	return (
+		<div className="wrapper">
+			<div className="nicknameContainer">
+				<Input placeholder="닉네임을 입력하세요....." onChange={event => handleInput(event)} />
+				<Button type="primary" onClick={onCreateNickname}>
+					확인
+				</Button>
+			</div>
+		</div>
+	);
 }
 
 export default SignUp;
