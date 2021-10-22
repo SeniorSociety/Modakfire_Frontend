c0cb446dde927c74c69b2ad5d52a586a6a37970c
소진수 <joshso121197@gmail.com>
2021-10-22 20:18:44 +0900

    [fix] config, signin

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,7 @@
 const BASE_URL = 'internal-BACK-Internal-ALB-1927362478.ap-northeast-2.elb.amazonaws.com';
 
 export const API = {
+	SIGN: BASE_URL,
 	GALLERIES: `${BASE_URL}/galleries`,
 	BOARD_VIEW: `${BASE_URL}/board-viewer`,
 	SIGNIN: `${BASE_URL}/users`,
