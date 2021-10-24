991518a2996c0d24f29aaaf67467f4e76a1037b5
nam2350 <nam2350@gmail.com>
2021-10-24 17:23:19 +0900

    posting view 기능 완성

--- src/pages/BoardList/BoardList.tsx
+++ src/pages/BoardList/BoardList.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import InfiniteScroll from '../../components/InfiniteScroll/InfiniteScroll';
 import './BoardList.scss';
 
 function BoardList() {
 	const history = useHistory();
-
+	const { id }: any = useParams();
 	return (
 		<div className="boardWrap">
 			<div className="buttonWrap">
 				<button className="like btn">즐겨찾기 추가</button>
-				<button className="write btn" onClick={() => history.push(`/board-post`)}>
+				<button className="write btn" onClick={() => history.push(`/board-post/${id}`)}>
 					게시글 작성하기
 				</button>
 			</div>
